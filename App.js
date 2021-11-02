5f61d8d96ab02ea88b5b28366c11f2f3a028b4c5
Daniel Cajiao <cajiao.dani@gmail.com>
2021-11-02 18:28:00 -0400

    Added Login and Register buttons on splash screen

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -10,12 +9,3 @@ export default function App() {
     <Login />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
