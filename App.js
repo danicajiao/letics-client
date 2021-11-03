ce8a70ac456ec1f8ce9c36e0250a571551611dd9
Man-doeh <uberlasagna@gmail.com>
2021-11-02 21:01:23 -0400

    Prepping for merge

--- App.js
+++ App.js
@@ -4,10 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 
 // Screens
 import Login from './screens/Login';
+import Workouts from './screens/Workouts';
 
 export default function App() {
   return (
-    <Login />
+    //<Login />
+    <Workouts />
   );
 }
 
