afbce6bc99d13e4734e32e5925183ce27581fbb2
Man-doeh <uberlasagna@gmail.com>
2021-11-06 01:40:01 -0400

    Navbar connects to complete pages and bottom tabs module from react navigation was installed

--- App.js
+++ App.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// for the Navigation Bar
+import { Ionicons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 // Screens
 import LoggedOut from './screens/LoggedOut';
 import Register from './screens/Register';
 import Login from './screens/Login';
-import Workouts from './screens/Workouts';
+import WorkoutLog from './screens/WorkoutLog';
 import Dashboard from './screens/Dashboard';
 
+// navigation bar component
+const Tab = createBottomTabNavigator();
+
+//function MyTabs() {
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Dashboard" component={Dashboard} options={{tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={24} color="black" />
+          ),}}/>
+      <Tab.Screen name="Workouts" component={WorkoutLog} options={{tabBarIcon: ({ color, size }) => (
+            <Ionicons name="barbell" size={24} color="black" />
+          ),}}/>
+    </Tab.Navigator>
+  );
+}
+
+// main
 export default function App() {
   return (
-    //<Register />
-    <Dashboard />
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
   );
 }
