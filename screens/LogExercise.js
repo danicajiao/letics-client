3f89a4446106b3ee436e5c65117c688eeab94baa
Daniel Cajiao <cajiao.dani@gmail.com>
2021-11-17 16:50:23 -0500

    Added minor code style changes

--- screens/LogExercise.js
+++ screens/LogExercise.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
-import {View, StyleSheet, Text, TouchableOpacity, TextInput} from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity, TextInput } from 'react-native';
 import { DataTable } from 'react-native-paper';
 import { Formik } from 'formik';
 import { Colors } from './../components/styles';
 
 let sets = [];
 
-function DisplaySets({props}) {
+function DisplaySets({ props }) {
     const list = () => {
         return sets.map((set) => {
             let weightString = 'set' + set.number + 'weight';
@@ -15,118 +15,118 @@ function DisplaySets({props}) {
             // console.log("---------")
             // console.log(weightString);
             // console.log(repsString);
-          return (
-              
-            <View key={set.number}>
-                <DataTable.Row>
-                    <DataTable.Cell>{set.number}</DataTable.Cell>
-                    <DataTable.Cell>
-                        <View style={styles.formArea}>
-                            <TextInput style={styles.inputArea}
-                                placeholder='0'
-                                onChangeText={props.handleChange(weightString)}
-                                onBlur={props.handleBlur(weightString)}
-                                value={props.values.weightString}
-                                keyboardType="number-pad"
-                            >
-                            </TextInput>
-                        </View>
-                    </DataTable.Cell>
-                    <DataTable.Cell>
-                        <View style={styles.formArea}>
-                            <TextInput style={styles.inputArea}
-                                placeholder='0'
-                                onChangeText={props.handleChange(repsString)}
-                                onBlur={props.handleBlur(repsString)}
-                                value={props.values.repsString}
-                                keyboardType="number-pad"
-                            >
-                            </TextInput>
-                        </View>
-                    </DataTable.Cell>
-                </DataTable.Row>
-            </View>
-          );
+            return (
+
+                <View key={set.number}>
+                    <DataTable.Row>
+                        <DataTable.Cell>{set.number}</DataTable.Cell>
+                        <DataTable.Cell>
+                            <View style={styles.formArea}>
+                                <TextInput style={styles.inputArea}
+                                    placeholder='0'
+                                    onChangeText={props.handleChange(weightString)}
+                                    onBlur={props.handleBlur(weightString)}
+                                    value={props.values.weightString}
+                                    keyboardType="number-pad"
+                                >
+                                </TextInput>
+                            </View>
+                        </DataTable.Cell>
+                        <DataTable.Cell>
+                            <View style={styles.formArea}>
+                                <TextInput style={styles.inputArea}
+                                    placeholder='0'
+                                    onChangeText={props.handleChange(repsString)}
+                                    onBlur={props.handleBlur(repsString)}
+                                    value={props.values.repsString}
+                                    keyboardType="number-pad"
+                                >
+                                </TextInput>
+                            </View>
+                        </DataTable.Cell>
+                    </DataTable.Row>
+                </View>
+            );
         });
-      };
-    
-      return <View>{list()}</View>;
+    };
+
+    return <View>{list()}</View>;
 }
 
-function LogExercise (){
+function LogExercise() {
 
     const [count, setCount] = useState(0);
 
-    function pushNewSet(){
-        if (sets.length < 3){
+    function pushNewSet() {
+        if (sets.length < 3) {
             sets.push(
                 {
-                   "number": sets.length + 1,
-                   "weight": 0,
-                   "reps": 0
+                    "number": sets.length + 1,
+                    "weight": 0,
+                    "reps": 0
                 }
             )
-    
+
             setCount(count + 1);
 
         }
     }
 
-        return (
-            <View style={styles.container}>
-                <TouchableOpacity style={styles.cancelBtn}></TouchableOpacity>
-                <TouchableOpacity style={styles.notesBtn}></TouchableOpacity>
-                <Text style={styles.exerciseTitle}>Random Exercise</Text>
-                <Formik
-                
-                    initialValues={
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.cancelBtn}></TouchableOpacity>
+            <TouchableOpacity style={styles.notesBtn}></TouchableOpacity>
+            <Text style={styles.exerciseTitle}>Random Exercise</Text>
+            <Formik
+
+                initialValues={
                     {
-                        set1weight: '', 
+                        set1weight: '',
                         set1reps: '',
-                        set2weight: '', 
+                        set2weight: '',
                         set2reps: '',
-                        set3weight: '', 
+                        set3weight: '',
                         set3reps: '',
-                        
+
                     }}
-                    onSubmit={(values) => {
-                        console.log(values);
-                    }}
-                >
-                    {(props) => (
-                        // CHECK THIS!!!!!!!!!!!!!!!
-                        <View style={styles.tableComponent}> 
+                onSubmit={(values) => {
+                    console.log(values);
+                }}
+            >
+                {(props) => (
+                    // CHECK THIS!!!!!!!!!!!!!!!
+                    <View style={styles.tableComponent}>
 
-                            <View style={styles.tableComponent}>
-                                <DataTable style={styles.table}>
-                                    <DataTable.Header>
-                                        <DataTable.Title>Set</DataTable.Title>
-                                        <DataTable.Title>Weight</DataTable.Title>
-                                        <DataTable.Title>Reps</DataTable.Title>
-                                    </DataTable.Header>
+                        <View style={styles.tableComponent}>
+                            <DataTable style={styles.table}>
+                                <DataTable.Header>
+                                    <DataTable.Title>Set</DataTable.Title>
+                                    <DataTable.Title>Weight</DataTable.Title>
+                                    <DataTable.Title>Reps</DataTable.Title>
+                                </DataTable.Header>
 
-                                    <DisplaySets props={props}/>
+                                <DisplaySets props={props} />
 
-                                </DataTable>
-                            </View>
-
-                            <TouchableOpacity style={styles.addSetBtn} onPress={pushNewSet}>
-                                <Text style={styles.addBtnText}>+ Add Set</Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity style={styles.logBtn} onPress={props.handleSubmit}>
-                                <Text style={styles.addBtnText}>LOG</Text>
-                            </TouchableOpacity>
-
+                            </DataTable>
                         </View>
-                    )}
+
+                        <TouchableOpacity style={styles.addSetBtn} onPress={pushNewSet}>
+                            <Text style={styles.addBtnText}>+ Add Set</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.logBtn} onPress={props.handleSubmit}>
+                            <Text style={styles.addBtnText}>LOG</Text>
+                        </TouchableOpacity>
+
+                    </View>
+                )}
 
 
 
-                </Formik>
-                
+            </Formik>
 
-            </View>
-        );
+
+        </View>
+    );
 
 }
 
@@ -141,7 +141,7 @@ function LogExercise (){
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-    },  
+    },
     cancelBtn: {
         position: 'absolute',
         backgroundColor: 'red',
